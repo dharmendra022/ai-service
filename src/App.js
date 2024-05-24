c3030c1fa2bbdcb4a62af673039e003d539dbf36
dharmendrasinghyadav <87720840+dharmendra022@users.noreply.github.com>
2024-05-24 22:51:43 +0530

    updated-code

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './assets/component/Home';
+import '../src/assets/css/style.css'
+import '../src/assets/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
